3f753cfe930cb3868d5905769500a26a14043e13
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2017-10-31 21:45:28 +0100

    Working...

--- grid.py
+++ grid.py
@@ -19,7 +19,7 @@ from animatai.mdp import MDP
 from animatai.utils import vector_add
 from animatai.agents import Agent, Obstacle, Thing, XYEnvironment
 from animatai.network import MotorNetwork, Network
-from animatai.network_rl import MotorModel, SensorModel, NetworkDP, NetworkQLearningAgent
+from animatai.network_rl import MotorModel, NetworkModel, NetworkDP, NetworkQLearningAgent
 
 
 # Setup logging
@@ -65,8 +65,8 @@ things = ('XXXXXX\n' +
           'XXXXXX')
 
 exogenous_things = ('      \n' +
-                    '     S\n' +
-                    '     W\n' +
+                    '    S \n' +
+                    '    W \n' +
                     '      \n' +
                     '      ')
 
@@ -78,21 +78,21 @@ OPTIONS = DotDict({
     'terrain': terrain.split('\n'),
     'things': things.split('\n'),
     'exogenous_things': exogenous_things.split('\n'),
-    'exogenous_things_prob': 0.005,
+    'exogenous_things_prob': 0.1,
     'objectives': {'energy': 1.0, 'water': 1.0},
     'rewards':{
         None: {
             Energy: {
-                'energy': 0.1,
-                'water': 0.0
+                'energy': 1.0,
+                'water': -0.001
             },
             Water: {
-                'energy': 0.0,
-                'water': 0.1
+                'energy': -0.001,
+                'water': 1.0
             },
             None: {
-                'energy': 0.0,
-                'water': 0.0
+                'energy': -0.001,
+                'water': -0.001
             }
         }
     },
@@ -116,6 +116,7 @@ class Grid(XYEnvironment):
         self.options = options
         self.options.ENV_ENCODING = [('l', Landmark), ('X', Obstacle),
                                      ('S', Energy), ('W', Water)]
+        self.options.save_history_for = [Energy, Water]
         super().__init__(self.options)
 
     def execute_action(self, agent, action, time):
@@ -126,6 +127,14 @@ class Grid(XYEnvironment):
         if not action in directions:
             l.error('execute_action:unknow action', action, 'for agent', agent, 'at time', time)
             return
+
+        if agent.location == (4, 1):
+            l.info('*** MIGHT HAVE FOUND ENERGY ***')
+            agent.location = agent_start_pos
+
+        if agent.location == (4, 2):
+            l.info('*** MIGHT HAVE FOUND WATER ***')
+            agent.location = agent_start_pos
 
         agent.bump = self.move_to(agent,
                                   vector_add(directions[action],
@@ -138,22 +147,8 @@ class Grid(XYEnvironment):
 # Motors and actions
 motors = ['^', 'v', '<', '>']
 north, south, east, west = frozenset([0]), frozenset([1]), frozenset([2]), frozenset([3])
-motors_to_action = {north: '^', south: 'v', east: '>', west: '<', '*': '-'}
-
-'''
-landmark_to_state = {frozenset([0]): 'a',
-                     frozenset([1]): 'b',
-                     frozenset([2]): 'c',
-                     frozenset([3]): 'd',
-                     frozenset([4]): 'e',
-                     frozenset([5]): 'f',
-                     frozenset([6]): 'g',
-                     frozenset([7]): 'h',
-                     frozenset([8]): 'i',
-                     frozenset([9]): 'j',
-                     frozenset([10]): 'k'}
-sensor_model = landmark_to_state
-'''
+motors_to_action = {north: '^', south: 'v', east: '>', west: '<'} #, '*': '-'}
+motor_model = MotorModel(motors_to_action)
 
 class GridAgent(Agent):
 
@@ -170,32 +165,33 @@ class GridAgent(Agent):
         water, energy = SENSOR(Water), SENSOR(Energy)
 
         # create one SENSOR for each square
-        landmark_sensor = []
+        sensor_dict = {}
         for lm in landmarks:
-            landmark_sensor.append(SENSOR(Landmark, lm))
+            sensor_dict[frozenset([SENSOR(Landmark, lm)])] = lm
+        network_model = NetworkModel(sensor_dict)
 
         M = MotorNetwork(motors, motors_to_action)
 
         # TODO: init=agent_start_pos, using a location here (only for debugging),
         #            is a state when MDP:s are used
-        #       motor_model=None using M.motors_for_all_actions() instead
-        #       sensor_model=None (used with MDP:s not proper environments)
-        self.ndp = NetworkDP(agent_start_pos, self.status, None, .9, None, M.motors_for_all_actions())
-        q_agent = NetworkQLearningAgent(self.ndp, Ne=5, Rplus=2,
+        #       sensor_model=None (used with MDPs but not in proper environments)
+        self.ndp = NetworkDP(agent_start_pos, self.status, motor_model, .9, network_model)
+        self.q_agent = NetworkQLearningAgent(self.ndp, Ne=5, Rplus=2,
                                         alpha=lambda n: 60./(59+n),
-                                        delta=0.5,
-                                        max_iterations=100)
+                                        delta=0.5)
 
 
         # compose applies the functions from right to left
         self.program = compose(do(partial(l.debug, 'mnetwork.update'))
                                , M.update
                                , do(partial(l.debug, 'q_agent'))
-                               , q_agent
+                               , self.q_agent
                                , do(partial(l.debug, N))
                                , do(partial(l.debug, 'network.update'))
                                , N.update
                                , do(partial(l.debug, 'percept'))
+                               , lambda x: do(partial(l.debug, '*** ENERY FOUND ***'))(x) if 'energy' in x[1] and x[1]['energy'] > 0.0 else x
+                               , lambda x: do(partial(l.debug, '*** WATER FOUND ***'))(x) if 'water' in x[1] and x[1]['water'] > 0.0 else x
                               )
 
     def __repr__(self):
@@ -204,6 +200,11 @@ class GridAgent(Agent):
 
 # Main
 # =====
+
+def print_grid(U):
+    l.info(U[0:4])
+    l.info(U[4:7])
+    l.info(U[7:11])
 
 def run(wss=None, steps=None, seed=None):
     steps = int(steps) if steps else 20
@@ -218,6 +219,31 @@ def run(wss=None, steps=None, seed=None):
     grid_agent = GridAgent(options.objectives, landmarks)
     grid.add_thing(grid_agent, agent_start_pos)
     grid.run(steps)
+
+    l.info('\n')
+    l.info('The Landmarks will have these numbers:')
+    l.info('   0, 1, 2, 3, 4, 5')
+    l.info('0: X, X, X, X, X, X')
+    l.info('1: X, 7, 8, 9,10, X')
+    l.info('2: X,13, X,15,16, X')
+    l.info('3: X,19,20,21,22, X')
+    l.info('4: X, X, X, X, X, X')
+
+    for status in ['energy', 'water']:
+        l.info('----- ' + status + '------')
+        U, pi = grid_agent.q_agent.Q_to_U_and_pi()[status]
+
+        l.info(U)
+        l.info(pi)
+
+        # print the utilities and the policy also
+        U1 = sorted(U.items(), key=lambda x: int(x[0] if type(x[0]) is str else 99))
+        pi1 = sorted(pi.items(), key=lambda x: int(x[0] if type(x[0]) is str else 99))
+        print_grid(U1)
+        print_grid(pi1)
+
+
+    l.info('q_agent:', grid_agent.q_agent)
 
 if __name__ == "__main__":
     run()
